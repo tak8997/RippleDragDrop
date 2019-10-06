a95dd04551b1dc013ed46a02e55d066936e3ffcf
Tak <tak8997@gmail.com>
2019-10-06 22:26:27 +0900

    ripple effect 작업 중

--- library/src/main/java/com/tak8997/library/RippleDragDropItem.kt
+++ library/src/main/java/com/tak8997/library/RippleDragDropItem.kt
@@ -16,12 +16,69 @@ internal class RippleDragDropItem @JvmOverloads constructor(
     var title = ""
     var tagTitle = ""
 
+    private val rippleScale = 6.0f
+    private val rippleDelay = 300
+    private val rippleDurationTime = 3000
+
+//    private val animators by lazy {
+//        mutableListOf<ObjectAnimator>().apply {
+//            repeat(3) { index ->
+//                val scaleXAnimator = ObjectAnimator.ofFloat(this, "ScaleX", 1.0f, rippleScale).apply {
+//                    repeatCount = ObjectAnimator.INFINITE
+//                    repeatMode = ObjectAnimator.RESTART
+//                    startDelay = (index * rippleDelay).toLong()
+//                    duration = rippleDurationTime.toLong()
+//                }
+//                add(scaleXAnimator)
+//
+//                val scaleYAnimator = ObjectAnimator.ofFloat(this, "ScaleY", 1.0f, rippleScale).apply {
+//                    repeatCount = ObjectAnimator.INFINITE
+//                    repeatMode = ObjectAnimator.RESTART
+//                    startDelay = (index * rippleDelay).toLong()
+//                    duration = rippleDurationTime.toLong()
+//                }
+//                add(scaleYAnimator)
+//
+//                val alphaAnimator = ObjectAnimator.ofFloat(this, "Alpha", 1.0f, 0f).apply {
+//                    repeatCount = ObjectAnimator.INFINITE
+//                    repeatMode = ObjectAnimator.RESTART
+//                    startDelay = (index * rippleDelay).toLong()
+//                    duration = rippleDurationTime.toLong()
+//                }
+//                add(alphaAnimator)
+//            }
+//        } as List<ObjectAnimator>
+//    }
+//
+//    private val animatorSet by lazy {
+//        AnimatorSet().apply {
+//            interpolator = AccelerateDecelerateInterpolator()
+//            playTogether(animators)
+//        }
+//    }
+//
+//    private val paint = Paint()
+
     init {
+//        paint.isAntiAlias = true
+//        paint.style = Paint.Style.FILL
+
         LayoutInflater.from(context).inflate(R.layout.item_ripple_drag_drop, this)
     }
 
+//    override fun dispatchDraw(canvas: Canvas?) {
+//        super.dispatchDraw(canvas)
+//
+//        val radius = width.coerceAtMost(height) / 2
+//        canvas?.drawCircle(radius.toFloat(), radius.toFloat(), (radius - 2.toPx()).toFloat(), paint)
+//    }
+
     fun setSelection(selected: Boolean) {
-        layoutSelected.visibility = if (selected) View.VISIBLE else View.INVISIBLE
+        layoutSelected.visibility = if (selected) {
+            View.VISIBLE
+        } else {
+            View.INVISIBLE
+        }
     }
 
     fun getSelection(): Boolean {
