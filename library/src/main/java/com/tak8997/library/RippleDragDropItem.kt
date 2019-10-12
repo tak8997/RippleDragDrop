5c07eec61eb868b49de0c4900b02ea881a6f217e
Tak <tak8997@gmail.com>
2019-10-13 01:49:21 +0900

    ripple effect animation적용

--- library/src/main/java/com/tak8997/library/RippleDragDropItem.kt
+++ library/src/main/java/com/tak8997/library/RippleDragDropItem.kt
@@ -16,68 +16,23 @@ internal class RippleDragDropItem @JvmOverloads constructor(
     var title = ""
     var tagTitle = ""
 
-    private val rippleScale = 6.0f
-    private val rippleDelay = 300
-    private val rippleDurationTime = 3000
-
-//    private val animators by lazy {
-//        mutableListOf<ObjectAnimator>().apply {
-//            repeat(3) { index ->
-//                val scaleXAnimator = ObjectAnimator.ofFloat(this, "ScaleX", 1.0f, rippleScale).apply {
-//                    repeatCount = ObjectAnimator.INFINITE
-//                    repeatMode = ObjectAnimator.RESTART
-//                    startDelay = (index * rippleDelay).toLong()
-//                    duration = rippleDurationTime.toLong()
-//                }
-//                add(scaleXAnimator)
-//
-//                val scaleYAnimator = ObjectAnimator.ofFloat(this, "ScaleY", 1.0f, rippleScale).apply {
-//                    repeatCount = ObjectAnimator.INFINITE
-//                    repeatMode = ObjectAnimator.RESTART
-//                    startDelay = (index * rippleDelay).toLong()
-//                    duration = rippleDurationTime.toLong()
-//                }
-//                add(scaleYAnimator)
-//
-//                val alphaAnimator = ObjectAnimator.ofFloat(this, "Alpha", 1.0f, 0f).apply {
-//                    repeatCount = ObjectAnimator.INFINITE
-//                    repeatMode = ObjectAnimator.RESTART
-//                    startDelay = (index * rippleDelay).toLong()
-//                    duration = rippleDurationTime.toLong()
-//                }
-//                add(alphaAnimator)
-//            }
-//        } as List<ObjectAnimator>
-//    }
-//
-//    private val animatorSet by lazy {
-//        AnimatorSet().apply {
-//            interpolator = AccelerateDecelerateInterpolator()
-//            playTogether(animators)
-//        }
-//    }
-//
-//    private val paint = Paint()
+    private val rippleEffect by lazy {
+        RippleEffect(context)
+    }
 
     init {
-//        paint.isAntiAlias = true
-//        paint.style = Paint.Style.FILL
-
         LayoutInflater.from(context).inflate(R.layout.item_ripple_drag_drop, this)
     }
 
-//    override fun dispatchDraw(canvas: Canvas?) {
-//        super.dispatchDraw(canvas)
-//
-//        val radius = width.coerceAtMost(height) / 2
-//        canvas?.drawCircle(radius.toFloat(), radius.toFloat(), (radius - 2.toPx()).toFloat(), paint)
-//    }
-
     fun setSelection(selected: Boolean) {
-        layoutSelected.visibility = if (selected) {
-            View.VISIBLE
+        if (selected) {
+            addView(rippleEffect)
+            rippleEffect.startRipple()
+            layoutSelected.visibility = View.VISIBLE
         } else {
-            View.INVISIBLE
+            rippleEffect.stopRipple()
+            removeView(rippleEffect)
+            layoutSelected.visibility = View.INVISIBLE
         }
     }
 
