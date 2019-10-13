b2b41fd20e32b41e99ab1900fda779bc0db223f7
Tak <tak8997@gmail.com>
2019-10-13 17:59:34 +0900

    fix deprecated ‘AndroidJUnit4’

--- sample/src/androidTest/java/com/tak8997/rippledragdrop/ExampleInstrumentedTest.kt
+++ sample/src/androidTest/java/com/tak8997/rippledragdrop/ExampleInstrumentedTest.kt
@@ -1,10 +1,11 @@
 package com.tak8997.rippledragdrop
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.runner.AndroidJUnit4
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 /**
  * Instrumented test, which will execute on an Android device.
