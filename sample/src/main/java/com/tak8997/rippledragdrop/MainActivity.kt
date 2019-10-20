d6a1ae707104428fb15d4c7d8e3bc2c2371722ef
ByungTak <tak8997@gmail.com>
2019-10-21 00:15:10 +0900

    드래그 된 곳까지 색 칠하기 (#9)

--- sample/src/main/java/com/tak8997/rippledragdrop/MainActivity.kt
+++ sample/src/main/java/com/tak8997/rippledragdrop/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tak8997.rippledragdrop
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        rootLayout.clipChildren = false
+        rootLayout.clipToPadding = false
     }
 }
